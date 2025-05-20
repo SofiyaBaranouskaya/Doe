70e0374f63278c52b3392e9a4aff7ac1734657bd
SofiyaBaranouskaya <sofi220307@gmail.com>
2025-05-20 19:59:32 +0300

    .

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['doe.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['doe-82da822924d4.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
