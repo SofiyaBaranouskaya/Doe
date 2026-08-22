cc0118cb3aa31c40591c0f1d3350861edffcb082
SofiyaBaranouskaya <sofi220307@gmail.com>
2026-08-22 17:46:39 +0300

    Clean up static storage config

--- config/settings.py
+++ config/settings.py
@@ -177,9 +177,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-# Для корректной работы whitenoise (если используешь)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
